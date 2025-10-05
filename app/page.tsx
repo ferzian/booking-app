30e80b72f12aa049c440f01d771f6104a72d3d26
ferzian <m.ferzian09@gmail.com>
2025-10-05 22:57:41 +0700

    Hero & Card Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Hero from "@/components/hero";
+import Main from "@/components/main";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <Hero />
+      <div className="mt-16">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold uppercase">Court & Rates</h1>
+          <p className="py-3">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid,
+            quia?
+          </p>
+        </div>
+        <Main />
+      </div>
     </div>
   );
 }
