59d40b2aae119abc64e38ff78ea476d7f8154c32
ferzian <m.ferzian09@gmail.com>
2025-10-05 21:04:14 +0700

    setup

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 w-full bg-white shadow-sm z-20">
+      <div className="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-4">
+        <Link href="/">
+          <Image src="/logo.png" width={128} height={49} alt="logo" priority />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
