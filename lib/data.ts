d884eb097dfdb1145a98dddbfd0592c9566a2ff0
ferzian <m.ferzian09@gmail.com>
2025-10-13 11:51:49 +0700

    Admin Dashboard

--- lib/data.ts
+++ lib/data.ts
@@ -137,3 +137,73 @@ export const getReservationByUserId = async () => {
     console.log(error);
   }
 };
+
+export const getRevenueAndReserve = async () => {
+  try {
+    const result = await prisma.reservation.aggregate({
+      _count: true,
+      _sum: { price: true },
+      where: {
+        Payment: { status: { not: "failure" } },
+      },
+    });
+    return {
+      revenue: result._sum.price || 0,
+      reserve: result._count,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getTotalCustomers = async () => {
+  try {
+    const result = await prisma.reservation.findMany({
+      distinct: ["userId"],
+      where: {
+        Payment: { status: { not: "failure" } },
+      },
+      select: { userId: true },
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getReservations = async () => {
+  const session = await auth();
+  if (
+    !session ||
+    !session.user ||
+    !session.user.id ||
+    session.user.role !== "admin"
+  ) {
+    throw new Error("Unauthorized Access");
+  }
+  try {
+    const result = await prisma.reservation.findMany({
+      include: {
+        Court: {
+          select: {
+            name: true,
+            image: true,
+            price: true,
+          },
+        },
+        User: {
+          select: {
+            name: true,
+            email: true,
+            phone: true,
+          },
+        },
+        Payment: true,
+      },
+      orderBy: { createdAt: "desc" },
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
